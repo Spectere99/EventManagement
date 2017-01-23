df671644430f4fe176d7cd9b60c5462d9e3a93c2
Spectere99 <rflowers@saber98.com>
2017-01-22 22:01:03 -0500

    Reorg

--- EventManagement/Controllers/AccountController.cs
+++ EventManagement/Controllers/AccountController.cs
@@ -303,32 +303,6 @@ namespace EventManagement.Controllers
             return View(model);
         }
 
-        [HttpGet]
-        [Authorize]
-        public ActionResult HouseHold()
-        {
-            PersonViewModel personViewModel = new PersonViewModel();
-            PersonReader personReader = new PersonReader();
-            var userId = User.Identity.GetUserId();
-            //Get person based on their user id
-            var person = personReader.GetByUserId(userId).FirstOrDefault();
-
-            if (person != null)
-            {
-                personViewModel = TranslatePersonDTO(person);
-                //Need to load the children now.
-                var children = personReader.GetByParentId(person.PersonId);
-                foreach (var child in children)
-                {
-                    var childViewModel = TranslatePersonDTO(child);
-
-                    personViewModel.Children.Add(childViewModel);
-                }
-                personViewModel.NotUnitAffiliated = person.Unit.UnitType.UnitTypeId.Equals(-1);
-            }
-
-            return View(personViewModel);
-        }
         //
         // GET: /Account/ConfirmEmail
         [AllowAnonymous]
@@ -609,33 +583,6 @@ namespace EventManagement.Controllers
                 return Redirect(returnUrl);
             }
             return RedirectToAction("Index", "Home");
-        }
-
-        private PersonViewModel TranslatePersonDTO(PersonDTO person)
-        {
-            PersonViewModel personViewModel = new PersonViewModel
-            {
-                FirstName = person.FirstName,
-                MiddleName = person.MiddleName,
-                LastName = person.LastName,
-                PersonType = person.PersonType.Type,
-                UnitType = person.Unit.UnitType.Type,
-                Unit = person.Unit.UnitNumber.ToString(),
-                UnitRank = person.Rank.Rank,
-                ContactInfo = new ContactInfoViewModel
-                {
-                    ContactName = person.ContactInfo.Name,
-                    Address1 = person.ContactInfo.Address1,
-                    Address2 = person.ContactInfo.Address2,
-                    CellPhone = person.ContactInfo.CellPhone,
-                    HomePhone = person.ContactInfo.HomePhone,
-                    City = person.ContactInfo.City,
-                    State = person.ContactInfo.State,
-                    Zip = person.ContactInfo.Zip
-                }
-            };
-
-            return personViewModel;
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
--- EventManagement/Controllers/PersonController.cs
+++ EventManagement/Controllers/PersonController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Common;
+using Common.Reader;
+using EventManagement.Models;
+using Microsoft.AspNet.Identity;
 
 namespace EventManagement.Controllers
 {
@@ -14,9 +18,60 @@ namespace EventManagement.Controllers
             return View();
         }
 
+        [HttpGet]
+        [Authorize]
         public ActionResult PersonDetails()
         {
-            return View();
+            PersonViewModel personViewModel = new PersonViewModel();
+            PersonReader personReader = new PersonReader();
+            var userId = User.Identity.GetUserId();
+            //Get person based on their user id
+            var person = personReader.GetByUserId(userId).FirstOrDefault();
+
+            if (person != null)
+            {
+                personViewModel = TranslatePersonDTO(person);
+                //Need to load the children now.
+                var children = personReader.GetByParentId(person.PersonId);
+                foreach (var child in children)
+                {
+                    var childViewModel = TranslatePersonDTO(child);
+
+                    personViewModel.Children.Add(childViewModel);
+                }
+                personViewModel.NotUnitAffiliated = person.Unit.UnitType.UnitTypeId.Equals(-1);
+            }
+
+            return View(personViewModel);
         }
+
+
+        private PersonViewModel TranslatePersonDTO(PersonDTO person)
+        {
+            PersonViewModel personViewModel = new PersonViewModel
+            {
+                FirstName = person.FirstName,
+                MiddleName = person.MiddleName,
+                LastName = person.LastName,
+                PersonType = person.PersonType.Type,
+                UnitType = person.Unit.UnitType.Type,
+                Unit = person.Unit.UnitNumber.ToString(),
+                UnitRank = person.Rank.Rank,
+                ContactInfo = new ContactInfoViewModel
+                {
+                    ContactName = person.ContactInfo.Name,
+                    Address1 = person.ContactInfo.Address1,
+                    Address2 = person.ContactInfo.Address2,
+                    CellPhone = person.ContactInfo.CellPhone,
+                    HomePhone = person.ContactInfo.HomePhone,
+                    City = person.ContactInfo.City,
+                    State = person.ContactInfo.State,
+                    Zip = person.ContactInfo.Zip
+                }
+            };
+
+            return personViewModel;
+        }
+
     }
 }
